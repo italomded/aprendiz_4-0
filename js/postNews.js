c582ab15fbfd7f76a593f853d02ed5e9797fbe5d
Ítalo Martins de Deus <italomddeus@gmail.com>
2022-12-23 12:43:50 -0300

    News js finished

--- js/postNews.js
+++ js/postNews.js
@@ -1,32 +1,48 @@
-(async () => {
-        const response = await fetch('https://api.github.com/repos/italomded/aprendiz_4-0/contents/md/news');
-        const data = await response.json();
+async function get() {
+    const response = await fetch('https://api.github.com/repos/italomded/aprendiz_4-0/contents/md/news');
+    return response.json();
+}
 
-        var html = [];
-        html.push(
-            '<div class="row justify-content-center">',
-            '<div class="col-10">',
-            '<div class="card">',
-            '<div class="card-body">'
-        );
-        const html_1 = html.join("");
+async function populate() {
+    const data = await this.get()
+    console.log(data);
 
-        html = [];
-        html.push(
-            'Error 404: File not found.',
-            '</md-block>',
-            '</div></div></div></div>'
-        );
-        const html_2 = html.join("");
+    let html = [];
+    html.push(
+        '<div class="row justify-content-center mb-3">',
+        '<div class="col-12 col-lg-10 justify-content-center">',
+        '<div class="card">',
+        '<div class="card-header">',
+        '<i class="fa-solid fa-envelope"></i>',
+        '</div>',
+        '<div class="card-body">'
+    );
+    const html_1 = html.join("");
 
-        const finalHtml = "";
+    html = [];
+    html.push(
+        'Error 404: File not found.',
+        '</md-block>',
+        '</div></div>',
+        '<div class="text-center mt-3">',
+        '<i class="fa-solid fa-diagram-next"></i>',
+        '</div></div></div>'
+    );
+    const html_2 = html.join("");
+
+    let finalHtml = "";
+    if (data.length > 5) {
+        for (let i = data.length - 5 ; i < data.length ; i++ ) {
+            finalHtml += html_1 + `<md-block src="${data[i].path}">` + html_2;
+        }
+    } else {
         for (let file of data) {
             finalHtml += html_1 + `<md-block src="${file.path}">` + html_2;
         }
-
-        document.getElementById('newsPlaceholder').innerHTML = finalHtml;
     }
-)()
+
+    document.getElementById('newsPlaceholder').innerHTML = finalHtml;
+}
 
 
 
--- md/news/post_2.md
+++ md/news/post_2.md
@@ -1,31 +1,4 @@
 # Iniecique flumine sentit opemque nomen viridique volant
-
-## Est multorum nomina cautus
-
-Lorem markdownum taurum aequore missus vana sic nostra, nymphae crescentesque
-per. Utraque nihil umoris vidit area, sudem fluit principio sunt? Amnis deos
-faciem modo illuc, ascendere fluctibus mihi Mavortis **exsiccata numine**,
-plaustrum pacali Phaethonta manu, aegide grave. Cruore exanimes arcus omnipotens
-traxit damna.
-
-## Editus Boeotiaque coeptis
-
-*Medium pudore*. Mea eram vocis placidis tendens turbant pallidaque, usque quod.
-Novat sociis, in silentia siccare; ulterius et cacumina Corythus. Vestis
-canibus. Et vati flectat, mihi si cedemus saepe merentem, sibi ante
-[raucaque](http://www.spemque.org/) vestibus.
-
-## Concidit timidissime utque occiduo
-
-Curvae laudamus radice ut praeceps corpore utque matre: per, vel Amphitryon
-quoque supernum. Lustrabere Thespiades [Lyrnesia cum
-lunares](http://www.natihaud.org/quam-pallene.aspx) videtis hunc turbine templi
-visa sidera multis venter manus. Nescio est siccis *parentes* Aeeta [simplicitas
-equos primus](http://thalamos.com/haerent-fine.aspx) sententia putet noctis
-remitti hinc turba dixit fratres accipe. Tantale terga, dat et, corpore tulimus
-qualem limite. Elusaque praeruptam turba congestos digitos capellae regione
-vates onus caputque loquentis, litore: hanc.
-
 ## At nuper Sardibus
 
 Indignantia simque et hunc, aures [ipse](http://quantumvinaque.io/in-est.html)
