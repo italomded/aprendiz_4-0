c6ccdf53dc806a6328c624fe7fd0b641c5216d31
Ítalo Martins de Deus <italomddeus@gmail.com>
2022-12-23 10:49:44 -0300

    News update

--- js/postNews.js
+++ js/postNews.js
@@ -2,23 +2,29 @@
         const response = await fetch('https://api.github.com/repos/italomded/aprendiz_4-0/contents/md/news');
         const data = await response.json();
 
-        let htmlString = '<div class="row justify-content-center">';
-        htmlString += '<div class="col-10">';
-        htmlString += '<div class="card">';
-        htmlString += '<div class="card-body">';
+        var html = [];
+        html.push(
+            '<div class="row justify-content-center">',
+            '<div class="col-10">',
+            '<div class="card">',
+            '<div class="card-body">'
+        );
+        const html_1 = html.join("");
 
+        html = [];
+        html.push(
+            'Error 404: File not found.',
+            '</md-block>',
+            '</div></div></div></div>'
+        );
+        const html_2 = html.join("");
+
+        const finalHtml = "";
         for (let file of data) {
-            htmlString += `<md-block src="${file.path}">`;
+            finalHtml += html_1 + `<md-block src="${file.path}">` + html_2;
         }
 
-        htmlString += 'Error 404: File not found.';
-        htmlString += '</md-block>';
-        htmlString += '</div>';
-        htmlString += '</div>';
-        htmlString += '</div>';
-        htmlString += '</div>';
-
-        document.getElementsByTagName('body')[0].innerHTML = htmlString;
+        document.getElementById('newsPlaceholder').innerHTML = finalHtml;
     }
 )()
 
